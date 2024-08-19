f21f013f57178ed060ad392df8ac0b5957bfa048
ojnode <ojinnakabu95@gmail.com>
2024-08-19 23:49:26 +0100

    move files

--- HMS.js
+++
@@ -1,6 +0,0 @@
-let patientRegistration = document.getElementById('patient-details')
-
-patientRegistration.addEventListener("submit", (e) => {
-    e.preventDefault();
-    console.log(document.getElementById("firstname").value)
-});
