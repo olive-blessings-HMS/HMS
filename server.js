b6e345d29528da16ae815b2f17729596121f50ba
ojnode <ojinnakabu95@gmail.com>
2024-08-20 23:50:15 +0100

    establish database connection

--- server.js
+++ server.js
@@ -2,13 +2,31 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const port = 3000;
+const mysql = require('mysql2');
+require('dotenv').config();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public'))); 
 
 app.post('/submit', (req, res) => {
-   const formdetails = req.body;
-   console.log('Form data received', formdetails);
+   const formDetails = req.body;
+   const connection = mysql.createConnection({
+      host: process.env.DB_HOST,
+      database: process.env.DB_DATABASE,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASS
+   });
+
+   connection.connect((error) => {
+      if(error) {
+         console.error("connection failed" + error.stack)
+         return;
+      }
+      console.log("connection successful")
+   });
+
+   // const address
+   console.log('Form data received', formDetails);
 });
 
 app.listen(port, () => {
