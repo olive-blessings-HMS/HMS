ee045ec4a6c9031796eca24b10425537f8c8bb48
ojnode <ojinnakabu95@gmail.com>
2024-08-29 21:51:00 +0100

    create functions for saving patient data into database

--- db.js
+++ db.js
@@ -1,6 +1,47 @@
 const mysql = require('mysql2');
 require('dotenv').config();
 
+class PatientAddress {
+    constructor({address, addressTwo, lga, state, stateOfOrigin}) {
+       this.address = address;
+       this.addressTwo = addressTwo;
+       this.lga = lga;
+       this.state = state;
+       this.stateOfOrigin = stateOfOrigin;
+    };
+
+    saveToDatabase() {
+        const query = 'INSERT INTO address (street_name, street_name_two, lga, state) Values (?,?,?,?)';
+        const values = [this.address, this.addressTwo, this.lga, this.state];
+        updatedb(query, values);
+    };
+};
+
+function applyFromdata(formDetails) {
+    const patientName = new PatientName(formDetails);
+    const patientAddress = new PatientAddress(formDetails);
+    console.log(`print addresstwo ${patientAddress.addressTwo}`);
+    patientAddress.saveToDatabase();
+};
+
+function updatedb(query, values = []) {
+    pool.getConnection(
+        (err, conn) => {
+        if (err) {
+            console.log('data base failed');
+            throw err;
+        }
+        conn.query(query, values, (err, results) => {
+            if (err) {
+                console.log(`error message ${err}`);
+            }
+            console.log(`${results}`);
+        });
+        conn.release;
+    });
+    
+};
+
 class PatientName {
     constructor({firstname, middlename, lastname}) {
        this.firstName = firstname;
@@ -11,7 +52,9 @@ class PatientName {
     getFullname() {
        return `${this.firstName} ${this.middleName} ${this.lastName}`;
     }
-}
+};
+
+
 
 const pool = mysql.createPool({
     host: process.env.DB_HOST,
@@ -20,20 +63,6 @@ const pool = mysql.createPool({
     password: process.env.DB_PASS
  });
 
- async function updatedb(formDetails) {
-    pool.getConnection(
-        (err, conn) => {
-        if (err) {
-            console.log('error getting connection ${err}');
-            return;
-        }
-        console.log('connection successful');
-        console.log('Form data received', formDetails);
-        const patient = new PatientName(formDetails);
-        console.log(patient.getFullname());
-        conn.release;
 
-    });
-}
 
-module.exports = updatedb;
+module.exports = applyFromdata;
