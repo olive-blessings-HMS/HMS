802f9f5dab8171839931a808f6935c35c8cee73e
ojnode <ojinnakabu95@gmail.com>
2024-09-20 23:51:26 +0100

    refactor: create new function for button creation use else if to for clarity

--- public/js/patientList.js
+++ public/js/patientList.js
@@ -1,5 +1,19 @@
 document.addEventListener('DOMContentLoaded', () => {
     const patientTable = document.getElementById('patientlist');
+    function createButton(value) {
+        const button = document.createElement('button');
+        button.className = "patientButton";
+        button.textContent = "Patient Details";
+        button.addEventListener('click', async function() {
+            const response = await fetch('/patientDetails', {
+                method: 'POST',
+                body: `${value}`,
+            })
+            const data = await response.json();
+            window.location.href = data.redirect;
+        });
+        return button;
+    }
     fetch('/patientlist')
     .then(response => {
         if (!response.ok) {
@@ -9,32 +23,19 @@ document.addEventListener('DOMContentLoaded', () => {
     })
     .then(patientlist => {
         for (let index = 0; index < patientlist.length; index++) {
-            patientDetails = Object.entries(patientlist[index])
+            let patientDetails = Object.entries(patientlist[index])
             patientDetails.push(patientDetails.shift());
             const tr = document.createElement("tr");
             for (const [key, value] of patientDetails) {
                 const td = document.createElement("td");
                 if (key === 'id') {
-                    const button = document.createElement('button');
-                    button.className = "patientButton";
-                    button.textContent = "Patient Details";
-                    button.addEventListener('click', async function() {
-                        const response = await fetch('/patientDetails', {
-                            method: 'POST',
-                            body: `${value}`,
-                        })
-                        const data = await response.json();
-                        window.location.href = data.redirect;
-                    });
-                    td.appendChild(button);
-                } else {
-                    if (key === 'dob' && value != null) {
+                    td.appendChild(createButton(value));
+                } else if (key === 'dob' && value != null) {
                         let endIndex = value.indexOf("T");
                         const newValue = value.slice(0, endIndex);
                         td.textContent = newValue;
-                    } else {
+                } else {
                         td.textContent = value;
-                    }
                 }
                 tr.appendChild(td);
             }
