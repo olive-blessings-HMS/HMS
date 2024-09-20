a47e30af91fc45fd7d84de54f2706dfa4aef2e9f
ojnode <ojinnakabu95@gmail.com>
2024-09-20 23:18:42 +0100

    use mysql session to improve security

--- server.js
+++ server.js
@@ -1,28 +1,46 @@
 const express = require('express');
+const mysql = require('mysql2');
+const session = require('express-session');
+const MySQLStore = require('express-mysql-session')(session);
+require('dotenv').config();
 const path = require('path');
-const { createPatientInfo, createSecContactInfo, saveToDatabase, retrieveFromDatabase } = require('./db');
+const { createPatientInfo, createSecContactInfo, saveToDatabase, 
+    retrieveFromDatabase, options} = require('./db');
 const app = express();
-const port = 3000;
+const port = 3000;  
+
+const pool = mysql.createPool(options);
+const sessionStore = new MySQLStore({}, pool);
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public'))); 
-
-let patientDetailsStore = {};
+app.use(express.text());
+app.use(session({
+    key: process.env.KEY1,
+    secret: process.env.SECRETKEY,
+    store: sessionStore,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {maxAge: 1000 * 60 * 60}
+}));
 
 app.post('/next', (req, res) => {
-    const formDetails = req.body;
-    patientDetailsStore = createPatientInfo(formDetails);
+    req.session.patientDetail = req.body;
     res.json({ redirect: '/html/secContact.html' });
 });
 
 app.post('/save', (req, res) => {
-    if (!patientDetailsStore) {
-        return res.status(400);
-    }
     const formDetails = req.body;
-    secContactDetails = createSecContactInfo(formDetails);
-    saveToDatabase(patientDetailsStore, secContactDetails);
-    patientDetailsStore = {};
+    const patientDetails = createPatientInfo(req.session.patientDetail);
+    const secContactDetails = createSecContactInfo(formDetails);
+    console.log("patient details", patientDetails);
+    console.log("secContact details", secContactDetails);
+    saveToDatabase(patientDetails, secContactDetails);
+    req.session.destroy((err) => {
+        if (err) {
+            return res.status(500);
+        }
+    });
     res.json({ redirect: '/html/homepage.html' });
 });
 
@@ -30,6 +48,12 @@ app.get('/patientlist', (req, res) => {
     retrieveFromDatabase((patientList) => {
         res.json(patientList);
     });
+});
+
+app.post('/patientDetails', (req, res) => {
+    const value = req.body;
+    const intValue = parseInt(value, 10);
+    res.json({redirect: '/html/patientDetails.html', pateintId: intValue});
 });
 
 app.listen(port, () => {
