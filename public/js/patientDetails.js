1387aedcde27349c9fee91add300d74870ae5959
ojnode <ojinnakabu95@gmail.com>
2024-10-03 22:48:34 +0100

    define button for updating patient info

--- public/js/patientDetails.js
+++ public/js/patientDetails.js
@@ -1,6 +1,16 @@
 document.addEventListener('DOMContentLoaded', () => {
     const patientInfoIds = [`#firstname`, `#middlename`, `#lastname`, `#gender`, 
     `#birthday`, `#occupation`, `#religion`, `#phonenumber`, `#email`, '#address']
+
+    const editButton = document.createElement('button');
+    editButton.textContent = 'Update';
+    editButton.id = 'editButton';
+
+    const patientProfile = document.getElementById('patient-profile');
+    patientProfile.insertBefore(editButton, patientProfile.lastChild);
+
+    // let isEditing = false;
+
     fetch('/patientDetails')
     .then(async response => {
         if (!response.ok) {
@@ -18,15 +28,16 @@ document.addEventListener('DOMContentLoaded', () => {
     .then(patientData => {
         for (let index in patientInfoIds) {
             let patientInfo = document.querySelector(patientInfoIds[index]);
-            let patientKey = patientInfoIds[index].substring(1);
+            let patientKey = patientInfoIds[index].substring(1); // Remove the # from the ID
             let patientInfoValue = patientData[patientKey];
             let paragraph = document.createElement('p');
             if (patientKey === 'birthday') {
-                patientInfoValue = patientInfoValue.split('T')[0];
+                patientInfoValue = patientInfoValue.split('T')[0]; // Remove Timestamp from database results
             }
             paragraph.textContent = patientInfoValue;
+            // paragraph.addEventListener('click', () => makeEditable(paragraph));
             patientInfo.appendChild(paragraph);
         }
-
     })
+
 })
