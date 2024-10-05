2aa1db1ce1d7bae946822a215612388e68e63f11
ojnode <ojinnakabu95@gmail.com>
2024-10-05 22:38:12 +0100

    configure node to listen on all network interfaces

--- server.js
+++ server.js
@@ -84,6 +84,6 @@ app.post('/updateDetails', (req, res) => {
     });
 });
 
-app.listen(port, () => {
+app.listen(port, '0.0.0.0', () => {
     console.log(`Example app listening at http://localhost:${port}`);
 });
