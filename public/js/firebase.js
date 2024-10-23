06a7c2f21f1a91e2474936c4c0c156b50d3b6df7
ojnode <ojinnakabu95@gmail.com>
2024-10-23 22:54:34 +0100

    remove properties not needed

--- public/js/firebase.js
+++ public/js/firebase.js
@@ -5,10 +5,7 @@ const firebaseConfig = {
     apiKey: "AIzaSyDXWA9cbObzKFddqebF34Ke9m6qhjkbMHw",
     authDomain: "hospital-90a89.firebaseapp.com",
     projectId: "hospital-90a89",
-    storageBucket: "hospital-90a89.appspot.com",
-    messagingSenderId: "409945300926",
     appId: "1:409945300926:web:d2cee5e2eda49520c3e3ef",
-    measurementId: "G-0QJCKL2DTZ"
   };
   
 
